fd5105cc55fad2345404e57dfb28ee6eef8abb7b
scatterbrain3 <scattini3@hotmail.com>
2019-11-10 22:53:17 +1030

    Added dynamic border color

--- src/Joke.js
+++ src/Joke.js
@@ -2,18 +2,44 @@ import React from 'react';
 import './Joke.css'
 
 class Joke extends React.Component{
-  render(){
-    return (
-      <div className="Joke">
-          <div className = "JokeButtons">
-            <i onClick={this.props.upvote} class="fas fa-arrow-up"></i>
-            <span className="JokeVote">{this.props.votes}</span>
-            <i onClick={this.props.downvote} class="fas fa-arrow-down"></i>
-          </div>
-          <div className="JokeText">{this.props.text}</div>
-      </div>
-    );
-  }
+
+    getColor(){
+        if(this.props.votes >= 15){
+            return "#4CAF50";
+        }
+        else if(this.props.votes >= 12){
+            return "#8BC34A"
+        }
+        else if(this.props.votes >= 9){
+            return "#CDDC39"
+        }
+        else if(this.props.votes >= 6){
+            return "#FFEB3B"
+        }
+        else if(this.props.votes >= 3){
+            return "#FFC107"
+        }
+        else if(this.props.votes >= 0){
+            return "#FF9800"
+        }
+        else{
+            console.log(this.props.votes)
+            return "#F44336"
+        }
+    }
+
+    render(){
+        return (
+        <div className="Joke">
+            <div className = "JokeButtons">
+                <i onClick={this.props.upvote} className="fas fa-arrow-up"></i>
+                <span className="JokeVote" style={{borderColor: this.getColor()}}>{this.props.votes}</span>
+                <i onClick={this.props.downvote} className="fas fa-arrow-down"></i>
+            </div>
+            <div className="JokeText">{this.props.text}</div>
+        </div>
+        );
+    }
 }
 
 export default Joke;
